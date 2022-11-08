dc9e07258382696e7c2bcb573473da6e40fda29e
Hector Gonzalez <gorozco.hector@gmail.com>
2022-11-08 11:02:46 -0600

    Make Icon component

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Portrait from '../assets/portrait.jpg';
+import Icon from './Icon';
 import '../styles/aboutMeStyle.scss';
 
 function AboutMe() {
@@ -8,25 +9,74 @@ function AboutMe() {
       <img src={Portrait} alt="Héctor González Orozco" />
       <h1>Héctor González Orozco</h1>
       <p>info</p>
-        <ul>
-          <li>
-            <h2>Languages</h2>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" alt="HTML 5" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" alt="CSS 3" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt="JavaScript" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" alt="TypeScript" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" alt="Python" />
-
-          </li>
-          <li>
-            <h2>Tools</h2>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" alt="Git" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="React" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="Redux" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" alt="Node" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="Jest" />
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <h2>Languages</h2>
+          <Icon text="Html">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"
+              alt="HTML 5"
+            />
+          </Icon>
+          <Icon text="Css">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"
+              alt="CSS 3"
+            />
+          </Icon>
+          <Icon text="Javascript">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
+              alt="JavaScript"
+            />
+          </Icon>
+          <Icon text="Typescript">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"
+              alt="TypeScript"
+            />
+          </Icon>
+          <Icon text="Python">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+              alt="Python"
+            />
+          </Icon>
+        </li>
+        <li>
+          <h2>Tools</h2>
+          <Icon text="Git">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg"
+              alt="Git"
+            />
+          </Icon>
+          <Icon text="React">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+              alt="React"
+            />
+          </Icon>
+          <Icon text="Redux">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+              alt="Redux"
+            />
+          </Icon>
+          <Icon text="Node">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg"
+              alt="Node"
+            />
+          </Icon>
+          <Icon text="Jest">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
+              alt="Jest"
+            />
+          </Icon>
+        </li>
+      </ul>
     </section>
   );
 }
---
+++ src/components/Icon.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+  position: relative;
+  border-radius: 50%;
+  background-color: var(--prussian-blue);
+  padding: 25px;
+  margin: 10px;
+  width: 50px;
+  height: 50px;
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: 0px 5px 10px 5px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+  transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  z-index: 0;
+
+  &:hover{
+    background-color: var(--dark-purple-2);
+  }
+
+  .tooltip {
+  position: absolute;
+  top: 0;
+  font-size: 1.5rem;
+  background: var(--dark-purple-2);
+  color: #ffffff;
+  padding: 8px;
+  border-radius: 1rem;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+  .tooltip::before {
+  position: absolute;
+  content: " ";
+  height: 8px;
+  width: 8px;
+  background: var(--dark-purple-2);
+  bottom: -3px;
+  left: 50%;
+  transform: translate(-50%) rotate(45deg);
+  transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+&:hover .tooltip {
+  top: -45px;
+  opacity: 1;
+  visibility: visible;
+  pointer-events: auto;
+}
+
+&:hover span,
+&:hover .tooltip {
+  text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+}
+`;
+
+interface IconProps {
+  children: React.ReactNode;
+  text: string;
+}
+
+function Icon({ children, text } : IconProps) {
+  return (
+    <Wrapper>
+      <span className="tooltip">{text}</span>
+      <span>{children}</span>
+    </Wrapper>
+  )
+};
+
+export default Icon;
